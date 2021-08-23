f9be0716872c97d6a37149760d400d0de8c20d71
NatySousa <nataliaspsousa@gmail.com>
2021-08-23 18:14:10 -0300

    GFT Teste 4

---
+++ GftTeste4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GftTeste4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TabelaDesconto t = new TabelaDesconto();
+
+            t.CalculcarDesconto();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GftTeste4/TAbelaDesconto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GftTeste4
+{
+    public class TabelaDesconto
+    {
+        private double _valorCompra;
+
+        public double ValorCompra
+        {
+            get { return _valorCompra; }
+            set { _valorCompra = value; }
+        }
+
+
+        private double __desconto;
+
+        public double Desconto
+        {
+            get { return __desconto; }
+            set { __desconto = value; }
+        }
+
+        public void CalculcarDesconto()
+        {
+            int valor = 300;
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine("Para compras a partir de " + valor + " desconto de " + i + "%");
+                valor = valor + 100;
+            }
+        }
+    }
+}
